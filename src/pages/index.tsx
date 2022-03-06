db8ad56de9739b4aaf1ef2325cdd5204b8ca5352
Gabriek0 <gabriel.sanches170@gmail.com>
2022-03-06 12:59:32 -0300

    styling home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,26 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { Header } from '../components/Header';
+import styles from './home.module.scss'
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>Set access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
