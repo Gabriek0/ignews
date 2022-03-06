dc17db5dc4f4fd167aa4cddc0e0651334242dd98
Gabriek0 <gabriel.sanches170@gmail.com>
2022-03-06 13:15:16 -0300

    styling subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { Header } from '../components/Header';
+import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss'
+import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
@@ -13,11 +15,13 @@ const Home: NextPage = () => {
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
-          <h1>News about the <span>React</span> world.</h1>
+          <h1>News about <br />the <span>React</span> world.</h1>
           <p>Set access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
+
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
