ee3f2f435b027279583aad0298ea0b7119c22c52
Gabriek0 <gabriel.sanches170@gmail.com>
2022-04-20 21:56:35 -0300

    adicionando navegacao de menus e o link do next com prefecth no componente header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import logo from '../../../public/images/logo.svg'
 import { SignInButton } from '../SignInButton'
+import Link from 'next/link';
 import styles from './styles.module.scss'
 
 export function Header() {
@@ -9,8 +10,12 @@ export function Header() {
             <div className={styles.headerContent}>
                 <Image src={logo} alt='logo' />
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    <Link href="/">
+                        <a className={styles.active}>Home</a>
+                    </Link>
+                    <Link href="/posts" prefetch>
+                        <a>Posts</a>
+                    </Link>
                 </nav>
 
                 <SignInButton />
