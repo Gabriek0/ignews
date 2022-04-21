c0978bdb674b0277a81c8228ff78beb69cc6ae41
Gabriek0 <gabriel.sanches170@gmail.com>
2022-04-20 22:08:31 -0300

    criando componente activelink para mudar a className da pagina ao clicar na aba do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,19 +3,22 @@ import logo from '../../../public/images/logo.svg'
 import { SignInButton } from '../SignInButton'
 import Link from 'next/link';
 import styles from './styles.module.scss'
+import { useRouter } from 'next/router';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <Image src={logo} alt='logo' />
                 <nav>
-                    <Link href="/">
+                    <ActiveLink activeClassName={styles.active} href="/">
                         <a className={styles.active}>Home</a>
-                    </Link>
-                    <Link href="/posts" prefetch>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
                         <a>Posts</a>
-                    </Link>
+                    </ActiveLink>
                 </nav>
 
                 <SignInButton />
