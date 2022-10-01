da327e96c780a0862a4bfd9bc70eb7d8047900d3
Gabriek0 <gabriel.sanches170@gmail.com>
2022-09-30 22:47:10 -0300

    feat: test SubscribeButton component

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,56 +1,49 @@
-import { signIn, useSession } from 'next-auth/react';
-import Router, { useRouter } from 'next/router';
-import { api } from '../../services/api';
-import { getStripeJs } from '../../services/stripe-js';
-import styles from './styles.module.scss'
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
+import styles from "./styles.module.scss";
 
-interface SubscribeButtonProps {
-    priceId: string;
-}
+export function SubscribeButton() {
+  const { data: session } = useSession();
+  const router = useRouter();
 
-export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-    const { data: session } = useSession();
-    const router = useRouter();
-
-
-    async function handleSubscribe() {
-        if (!session) {
-            signIn();
-            return;
-        }
-
-        if (session.activeSubscription) {
-            router.push('/posts');
-            return;
-        };
-
-        // create checkout session
-        try {
-            const response = await api.post('/subscribe')
-
-            const { sessionId } = response.data;
-
-            const stripe = await getStripeJs();
-
-            await stripe!.redirectToCheckout({ sessionId })
-
-        } catch (err) {
-            let errorMessage = 'Failed';
-            if (err instanceof Error) {
-                errorMessage = err.message;
-            }
-            alert(errorMessage);
-        }
+  async function handleSubscribe() {
+    if (!session) {
+      signIn();
+      return;
     }
 
-    return (
-        <button
-            type='button'
-            className={styles.subscribeButton}
-            onClick={handleSubscribe}
-        >
-            Subscribe now
-        </button>
+    if (session.activeSubscription) {
+      router.push("/posts");
+      return;
+    }
 
-    )
+    // create checkout session
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe!.redirectToCheckout({ sessionId });
+    } catch (err) {
+      let errorMessage = "Failed";
+      if (err instanceof Error) {
+        errorMessage = err.message;
+      }
+      alert(errorMessage);
+    }
+  }
+
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  );
 }
