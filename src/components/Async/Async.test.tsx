12c17bf9a58c3257b184da8073c65c5d41c693b4
Gabriek0 <gabriel.sanches170@gmail.com>
2022-10-07 14:20:16 -0300

    feat: add async tests

---
+++ src/components/Async/Async.test.tsx
@@ -0,0 +1,22 @@
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { Async } from ".";
+
+test("it renders correctly", async () => {
+  render(<Async />);
+
+  expect(screen.getByText("Hello World")).toBeInTheDocument();
+
+  // await waitForElementToBeRemoved(screen.queryByText("Button"));
+
+  /*
+  await waitFor(() => {
+    return expect(screen.queryByText("Button")).not.toBeInTheDocument();
+  });*/
+
+  expect(await screen.findByText("Button")).toBeInTheDocument();
+});
